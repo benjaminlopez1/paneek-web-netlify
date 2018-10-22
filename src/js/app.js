ea3a2d58bcedb2a62a5c4f89b240ac69cc0c0f93
Kevin Moreno <kevinsmb11@gmail.com>
2018-10-22 13:27:13 -0400

    compress img, lazy load, preload, responsive

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,10 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+	img.setAttribute('src', img.getAttribute('data-src'));
+	img.onload = function() {
+		img.removeAttribute('data-src');
+	};
+});
